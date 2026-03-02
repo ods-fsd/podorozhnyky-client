f6001cd54a71e3ee01bb98a16c6b1b9a71cad5f3
Vladyslav Zaporozhets <ziper.step@gmail.com>
2026-03-02 20:17:28 +0100

    fix(validation): виправлено помилку ініціалізації аватара та додано ліміти за ТЗ (#52)
    * fix(UI): приховано кнопку 'Опублікувати Історію' для неавторизованих користувачів
    
    * fix(validation): виправлено помилку ініціалізації аватара та додано ліміти за ТЗ

--- src/schemas/profileSchemas.ts
+++ src/schemas/profileSchemas.ts
@@ -5,17 +5,26 @@ const MAX_AVATAR_SIZE = 500 * 1024;
 export const profileEditSchema = Yup.object({
   name: Yup.string()
     .min(2, "Мінімум 2 символи")
-    .max(32, "Максимум 32 символи"),
+    .max(32, "Максимум 32 символи")
+    .required("Ім'я обов'язкове"),
+
   email: Yup.string()
     .email("Невірний формат email")
-    .max(64, "Максимум 64 символи"),
-  description: Yup.string().max(150, "Максимум 150 символів"),
-  avatar: Yup.mixed().test(
-    "fileSize",
-    "Розмір файлу не має перевищувати 500kB",
-    (value) => {
-      if (!value) return true; 
-      return (value as File).size <= MAX_AVATAR_SIZE;
-    },
-  ),
+    .max(64, "Максимум 64 символи")
+    .required("Email обов'язковий"),
+
+  description: Yup.string()
+    .max(150, "Максимум 150 символів"),
+    
+
+  avatar: Yup.mixed()
+    .nullable()
+    .test("fileSize", "Розмір файлу не має перевищувати 500kB", (value) => {
+      if (!value || !(value instanceof File)) return true;
+      return value.size <= MAX_AVATAR_SIZE;
+    })
+    .test("fileType", "Дозволені формати: JPG, PNG, WEBP", (value) => {
+      if (!value || !(value instanceof File)) return true;
+      return ["image/jpeg", "image/png", "image/webp"].includes(value.type);
+    }),
 });
