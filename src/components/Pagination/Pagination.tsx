724f0c95cb82b1d7cbcc60a19d7480995886f135
Yaroslav Bilyk <bilyk.yaroslav.aleksandrovich@gmail.com>
2026-03-04 19:38:13 +0200

    logik (#63)

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,9 +2,9 @@ import ReactPagination from "react-paginate";
 import css from "./Pagination.module.css";
 
 interface PaginationProps {
-  totalPages: number; // Загальна кількість сторінок
-  currentPage: number; // Поточна активна сторінка
-  onPageChange: (page: number) => void; // Функція, що спрацьовує при кліку на сторінку
+  totalPages: number; 
+  currentPage: number; 
+  onPageChange: (page: number) => void; 
 }
 
 export default function Pagination({
@@ -15,12 +15,12 @@ export default function Pagination({
   return (
     <ReactPagination
       pageCount={totalPages}
-      pageRangeDisplayed={5} // Скільки кнопок сторінок показувати посередині
-      marginPagesDisplayed={1} // Скільки кнопок показувати по краях (наприклад, 1 ... 4 5 6 ... 10)
-      onPageChange={({ selected }) => onPageChange(selected + 1)} // react-paginate рахує з 0, тому додаємо 1
-      forcePage={currentPage - 1} // Вказуємо активну сторінку (знову ж таки, мінус 1 через відлік з нуля)
-      containerClassName={css.pagination} // Клас для <ul>
-      activeClassName={css.active} // Клас для активної <li>
+      pageRangeDisplayed={5} 
+      marginPagesDisplayed={1} 
+      onPageChange={({ selected }) => onPageChange(selected + 1)} 
+      forcePage={currentPage - 1} 
+      containerClassName={css.pagination} 
+      activeClassName={css.active} 
       nextLabel="→"
       previousLabel="←"
     />
