105c5e4692c44099458e3a6dbb70fbb8a6a9ea72
unknown <mykola.kruhlyk.pl@gmail.com>
2026-03-04 19:35:28 +0100

    Applied formatting fixes and small style improvements

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -99,7 +99,7 @@ export default function MobileMenu({
           <div className={css.authButtonsWrapper}>
             <div className={css.authContent}>
               <div className={finalStoryButtonEl}>
-                {isAuthenticated && ( ///Vlad виправлення кнопки
+                {isAuthenticated && ( 
                   <Link
                     href="/stories/create"
                     className={css.storyLinkElMob}
